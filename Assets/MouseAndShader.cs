6a33a6e6deeeb77a9a839ddc598e1a199883fad2
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-02-09 19:16:47 +0530

    Almost Done with logic

--- Assets/MouseAndShader.cs
+++ Assets/MouseAndShader.cs
@@ -11,6 +11,11 @@ public class MouseAndShader : MonoBehaviour
 
     float valX, valY;
 
+    private IEnumerator storeRoutine;
+    float timeStore = 0;
+    bool isRunning = false;
+    bool timerRunning = false;
+
     private void Awake()
     {
         material = GetComponent<Renderer>().material;
@@ -21,6 +26,7 @@ public class MouseAndShader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        storeRoutine = TestRoutine();
     }
 
     // Update is called once per frame
@@ -28,55 +34,61 @@ public class MouseAndShader : MonoBehaviour
     {
         if (Input.GetAxis("Mouse X") != 0)
         {
-            valX = valX + Input.GetAxis("Mouse X") * Time.deltaTime * 10;
-            Debug.Log(valX);
+            timerRunning = true;
+            if(!isRunning)
+            {
+                //valX = valX + Input.GetAxis("Mouse X") * Time.deltaTime * 10;
+                StartCoroutine(storeRoutine);
+                isRunning = true;
+            }
         }
-        else
+        else if (Input.GetAxis("Mouse X") == 0 && isRunning)
         {
-            valX = 0;
-            //if (valX > threshold)
-            //{
-            //    valX -= 0.5f * Time.deltaTime;
-            //}
-            //else if (valX < -threshold)
-            //{
-            //    valX += 0.5f * Time.deltaTime;
-            //}
-            //else if (Mathf.Abs(valX) < threshold)
-            //{
-            //    valX = 0;
-            //}
+            timerRunning = false;
+            if(timeStore + 2f < Time.time)
+            {
+                StopCoroutine(storeRoutine);
+                isRunning = false;
+                Debug.Log("!!!");
+                valX = 0;
+            }    
         }
-        if (Input.GetAxis("Mouse Y") != 0)
+
+        if(timerRunning && isRunning)
         {
-            valY = valY + Input.GetAxis("Mouse Y") * Time.deltaTime * 10;
+            timeStore = Time.time;
         }
-        else
-        {
-            valY = 0;
-            //if (valY > threshold)
-            //{
-            //    valY -= 0.5f * Time.deltaTime;
-            //}
-            //else if (valY < -threshold)
-            //{
-            //    valY += 0.5f * Time.deltaTime;
-            //}
-            //else if (Mathf.Abs(valY) < threshold)
-            //{
-            //    valY = 0;
-            //}
-        }
-        Debug.Log(valX + " valY: " + valY);
+
+        //Debug.Log(valX + " valY: " + valY);
         material.SetFloat("_DirectionX", Mathf.Clamp(valX, -1, 1));
         //material.SetFloat("_DirectionY", Mathf.Clamp(valY, -1, 1));
     }
 
+    IEnumerator TestRoutine()
+    {
+        while (true)
+        {
+            valX += 0.01f;
+            Debug.Log("+++++" + valX);
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+    IEnumerator StopOtherRoutine()
+    {
+        yield return new WaitForSeconds(2f);
+        if (Input.GetAxis("Mouse X") == 0)
+        {
+            StopCoroutine(storeRoutine);
+            Debug.Log("!!!");
+            valX = 0;
+        }
+    }
+
     IEnumerator AnimateProperties()
     {
-        while(true)
+        while (true)
         {
-            if(Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+            if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
             {
                 material.SetFloat("_DirectionX", 1);
                 material.SetFloat("_DirectionY", 1);
